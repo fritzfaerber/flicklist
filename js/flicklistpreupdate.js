0a9474b7b6f87134fae4beca8fd3c3065dc504c8
Fritz <fritzfaerber@yahoo.com>
2016-10-20 17:59:45 -0500

    updated studio 7

--- js/flicklistpreupdate.js
+++ js/flicklistpreupdate.js
@@ -40,28 +40,36 @@ function discoverMovies(callback) {
  * re-renders the page with new content, based on the current state of the model
  */
 function render() {
-    $('#section-watchlist ul').empty();
+    $('#section-watchlist ul').innerHTML = '';
     $('#section-browse ul').empty();
 
   // TODO 6
   // for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section
     model.watchlistItems.forEach(function(movie){
-        var itemView = $("<li></li>").text(movie.original_title);
-  	    $("#section-watchlist ul").append(itemView);;
+        $('#section-watchlist ul').append('<li>' + movie + '</li>');
     });
     model.browseItems.forEach(function(movie){
         //$('#section-browse ul').append('<li> + movie.title + </li>');
-        var title = $('<p></p>').text(movie.original_title);
-        var itemView = $("<li></li>").append(title)
-        $("#section-browse ul").append(itemView);
-
-        var button = $("<button></button>").text("Add to Watchlist").click(function() {
+        var button = $('<button> Add to Watch List</button>');
+        button.click(function(){
             model.watchlistItems.push(movie);
             render();
         });
-        itemView.append(button);
-    });
+        var listItem = $('<li><span>' + movie.title + '</span></li>');
+        listItem.append(button);
+        $('#section-browse ul').append(listItem);
 
+  // for each movie on the current browse list,
+        
+		// TODO 3
+		// insert a list item into the <ul> in the browse section
+
+		// TODO 4
+		// the list item should include a button that says "Add to Watchlist"
+
+		// TODO 5
+		// when the button is clicked, this movie should be added to the model's watchlist and render() should be called again
+  });
 
 }
 
